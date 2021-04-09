e1c0af36d94e0475285a80c0994c6ade74c8cca1
Muhammad-MO <muhdmahmood@gmail.com>
2021-04-09 05:34:22 +0000

    migration of models.py  database carried out

--- healthfood/models.py
+++ healthfood/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class healthfood(models.Model):
+    title = models.CharField(blank=False, max_length=255)
+    ISBN = models.CharField(blank=False, max_length=255)
+    decs = models.TextField(blank=False)
+
+
+def __str__(self):
+
+    return self.title
