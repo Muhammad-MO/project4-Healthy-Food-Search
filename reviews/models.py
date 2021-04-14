0f676f80e8eba118157303d85803fb089e33e21d
Muhammad-MO <muhdmahmood@gmail.com>
2021-04-14 09:49:16 +0000

    Add review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+
+
+class reviews(models.Model):
+    name = models.CharField(blank=False, max_length=255)
+    Email = models.EmailField(blank=False, max_length=320)
+    date = models.DateField(default=datetime.date.today)
+    reviews = models.TextField(blank=False)
+
+
+def __str__(self):
+    return self.title
