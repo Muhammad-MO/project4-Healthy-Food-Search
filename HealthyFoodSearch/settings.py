3395d1e3d64918be59f70a677530cb866de3426a
Muhammad-MO <muhdmahmood@gmail.com>
2021-04-18 10:17:04 +0000

    downloaded auth templates

--- HealthyFoodSearch/settings.py
+++ HealthyFoodSearch/settings.py
@@ -73,7 +73,8 @@ ROOT_URLCONF = 'HealthyFoodSearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
