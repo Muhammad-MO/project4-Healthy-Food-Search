5b8c1ccff8a6965bb9a843752a070215116b6f40
Muhammad-MO <muhdmahmood@gmail.com>
2021-05-07 22:55:30 +0000

    modified CSS styles for all templates

--- healthfood/views.py
+++ healthfood/views.py
@@ -59,12 +59,12 @@ def create_healthfood(request):
             return redirect(reverse(index))
         else:
             # 5. if does not have valid values, re-render the form
-            return render(request, 'healthfood/create.template.html', {
+            return render(request, 'healthfood/create-template.html', {
                 'form': create_form
             })
     else:
         create_form = healthfoodForm()
-        return render(request, 'healthfood/create.template.html', {
+        return render(request, 'healthfood/create-template.html', {
             'form': create_form
         })
 
