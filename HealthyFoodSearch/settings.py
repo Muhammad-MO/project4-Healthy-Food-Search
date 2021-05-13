05713d8d12d221f9b3a70cff85ca7dffd522212e
Muhammad-MO <muhdmahmood@gmail.com>
2021-05-13 17:32:35 +0000

    changed landing page design layout

--- HealthyFoodSearch/settings.py
+++ HealthyFoodSearch/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+from dotenv import load_dotenv, dotenv_values
 import os
 import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,10 +131,8 @@ WSGI_APPLICATION = 'HealthyFoodSearch.wsgi.application'
 # }
 # }
 
-DATABASES = {
+DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
 
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
