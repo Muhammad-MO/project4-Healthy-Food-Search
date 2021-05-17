dfeeffbf799a2ad55474963a5f742ae27a9d4d46
Muhammad-MO <muhdmahmood@gmail.com>
2021-05-17 01:51:02 +0000

    updated email settings in settings.py

--- HealthyFoodSearch/settings.py
+++ HealthyFoodSearch/settings.py
@@ -123,7 +123,7 @@ else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = "smtp.gmail.comm"
+    EMAIL_HOST = "smtp.gmail.com"
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
